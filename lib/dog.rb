1f8f9a936b60c92c0b2c7f20fa0f218588aca3aa
Berry  Semexan <bsemexan@gmail.com>
2018-02-08 18:14:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog
+  def bar
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
 end
 
-def bar
-  puts "Woof!"
-end
 
-def sit
-  puts "The Dog is sitting"
-end
-
-Dog.bark
-
-Dog.sit
